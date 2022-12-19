f8e7e1578fd9ef49485bc3592ff5a4718611a747
Dzmitry Charnavoki <Dzmitry_Charnavoki@epam.com>
2022-12-19 20:59:55 +0100

    70

---
+++ src/main/java/problems/p0000_0100/p0070_climbing_stairs/Solution.java
@@ -0,0 +1,34 @@
+package problems.p0000_0100.p0070_climbing_stairs;
+
+class Solution {
+    int[] memo = new int[46];
+    int r = 0;
+
+    public int climbStairs(int n) {
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int r = climbStairs(n - 1) + climbStairs(n - 2);
+        memo[n] = r;
+        return r;
+    }
+
+    //  Bottom — Up Approach
+    public int climbStairsBottomUp(int n) {
+        int prev = 1;
+        int temp = 0;
+        int cur = 1;
+        for (int i = 2; i <= n; i++) {
+            temp = cur;
+            cur = cur + prev;
+            prev = temp;
+        }
+        return cur;
+    }
+}
