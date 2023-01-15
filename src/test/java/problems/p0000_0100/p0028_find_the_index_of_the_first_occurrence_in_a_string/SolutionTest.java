87706ae2e06c1814180e4b69883ebe73ea30a59e
Dzmitry Charnavoki <Dzmitry_Charnavoki@epam.com>
2023-01-15 16:44:20 +0100

    ci test

---
+++ src/test/java/problems/p0000_0100/p0028_find_the_index_of_the_first_occurrence_in_a_string/SolutionTest.java
@@ -0,0 +1,39 @@
+package problems.p0000_0100.p0028_find_the_index_of_the_first_occurrence_in_a_string;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    void strStr1() {
+        assertEquals(0, new Solution().strStr("sadbutsad", "sad"));
+    }
+
+    @Test
+    void strStr2() {
+        assertEquals(-1, new Solution().strStr("leetcode", "leeto"));
+    }
+
+    @Test
+    void strStr3() {
+        assertEquals(-1, new Solution().strStr("code", "leetcode"));
+    }
+
+    @Test
+    void strStr31() {
+        assertEquals(0, new Solution().strStr("code", ""));
+    }
+
+    @Test
+    void strStr4() {
+        assertEquals(3, new Solution().strStr("Darova", "ov"));
+    }
+
+    @Test
+    void strStr5() {
+        assertEquals(5, new Solution().strStr("avdsvfbbrethjq", "f"));
+    }
+
+}
