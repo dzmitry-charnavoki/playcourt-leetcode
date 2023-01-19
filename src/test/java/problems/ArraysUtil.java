83334593fad2d969914f3ba36b3965f12d199d77
Dzmitry Charnavoki <Dzmitry_Charnavoki@epam.com>
2023-01-19 09:55:32 +0100

    update version of plugin

--- src/test/java/problems/ArraysUtil.java
+++ src/test/java/problems/ArraysUtil.java
@@ -15,5 +15,13 @@ public final class ArraysUtil {
         }
         return true;
     }
+    public static boolean assertCharactersArrayEquals(char[][] exp, char[][] cur){
+        for (int i = 0; i < exp.length; i++) {
+            char[] e = exp[i];
+            char[] c = cur[i];
+            Assertions.assertArrayEquals(e, c);
+        }
+        return true;
+    }
 
 }
