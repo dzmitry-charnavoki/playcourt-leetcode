32cb21188831ca9bc0e91e332aa1a73287cf82a4
Dzmitry Charnavoki <Dzmitry_Charnavoki@epam.com>
2023-01-21 21:38:33 +0100

    36. Valid Sudoku

--- src/main/java/problems/p0000_0100/p0036_valid_sudoku/Solution.java
+++ src/main/java/problems/p0000_0100/p0036_valid_sudoku/Solution.java
@@ -6,16 +6,19 @@ import java.util.Set;
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         Set<String> seen = new HashSet<>();
-        for (int i = 0; i < 9; ++i)
+        for (int i = 0; i < 9; ++i) {
             for (int j = 0; j < 9; ++j) {
-                if (board[i][j] == '.')
+                if (board[i][j] == '.') {
                     continue;
+                }
                 final char c = board[i][j];
                 if (!seen.add(c + "@row" + i) ||
                     !seen.add(c + "@col" + j) ||
-                    !seen.add(c + "@box" + i / 3 + j / 3))
+                    !seen.add(c + "@box" + i / 3 + j / 3)) {
                     return false;
+                }
             }
+        }
 
         return true;
     }
--- src/test/java/problems/p0000_0100/p0036_valid_sudoku/SolutionTest.java
+++ src/test/java/problems/p0000_0100/p0036_valid_sudoku/SolutionTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
+
 class SolutionTest {
 
     @Test
