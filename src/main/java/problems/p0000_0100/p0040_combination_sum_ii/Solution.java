f35e502ad288c3a7a424e28cadf7020a788141b3
Dzmitry Charnavoki <Dzmitry_Charnavoki@epam.com>
2023-01-29 00:10:49 +0100

    migration to gmavenplus

--- src/main/java/problems/p0000_0100/p0040_combination_sum_ii/Solution.java
+++ src/main/java/problems/p0000_0100/p0040_combination_sum_ii/Solution.java
@@ -16,16 +16,19 @@ class Solution {
 
     private void dfs(int s, int[] candidates, int target, List<Integer> path,
                      List<List<Integer>> ans) {
-        if (target < 0)
+        if (target < 0) {
             return;
+        }
+
         if (target == 0) {
             ans.add(new ArrayList<>(path));
             return;
         }
 
         for (int i = s; i < candidates.length; ++i) {
-            if (i > s && candidates[i] == candidates[i - 1])
+            if (i > s && candidates[i] == candidates[i - 1]) {
                 continue;
+            }
             path.add(candidates[i]);
             dfs(i + 1, candidates, target - candidates[i], path, ans);
             path.remove(path.size() - 1);
