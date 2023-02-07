3a015af4d193e06ff76beb20dc2f1f17b8ea8ef2
Dzmitry Charnavoki <Dzmitry_Charnavoki@epam.com>
2023-02-07 09:52:02 +0100

    136. Single Number

--- src/main/java/problems/p0100_0200/p0136_single_number/Solution.java
+++ src/main/java/problems/p0100_0200/p0136_single_number/Solution.java
@@ -1,6 +1,6 @@
 package problems.p0100_0200.p0136_single_number;
 
-public class Solution {
+class Solution {
     public int singleNumber(int[] nums) {
         int ans = 0;
         for (int n : nums) {
