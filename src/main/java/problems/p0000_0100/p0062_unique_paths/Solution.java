04aecc3209f10d6e479e6bb726abfc7659ae2777
Dzmitry Charnavoki <Dzmitry_Charnavoki@epam.com>
2023-03-03 17:57:01 +0100

    62. Unique Paths

---
+++ src/main/java/problems/p0000_0100/p0062_unique_paths/Solution.java
@@ -0,0 +1,41 @@
+package problems.p0000_0100.p0062_unique_paths;
+
+class Solution {
+
+    // top-down
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    // bottom-up
+    // does not fit here due exceed time of execution
+    // to slow? stack too big?
+    public int uniquePaths2(int m, int n) {
+        return up(m - 1, n - 1);
+    }
+
+    private int up(int i, int j) {
+        if (i == 0 || j == 0) {
+            return 1;
+        }
+        return up(i, j - 1) + up(i - 1, j);
+    }
+
+
+
+}
---
+++ src/test/java/problems/p0000_0100/p0062_unique_paths/SolutionTest.java
@@ -0,0 +1,37 @@
+package problems.p0000_0100.p0062_unique_paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    void uniquePaths1() {
+        assertEquals(28, new Solution().uniquePaths(3, 7));
+    }
+
+    @Test
+    void uniquePaths2() {
+        assertEquals(3, new Solution().uniquePaths(2, 3));
+    }
+    @Test
+    void uniquePaths3() {
+        assertEquals(1916797311, new Solution().uniquePaths(51, 9));
+    }
+
+    @Test
+    void uniquePaths_s1() {
+        assertEquals(28, new Solution().uniquePaths2(3, 7));
+    }
+
+    @Test
+    void uniquePaths_s2() {
+        assertEquals(3, new Solution().uniquePaths2(2, 3));
+    }
+
+    @Test
+    void uniquePaths_s3() {
+        assertEquals(1916797311, new Solution().uniquePaths2(51, 9));
+    }
+}
