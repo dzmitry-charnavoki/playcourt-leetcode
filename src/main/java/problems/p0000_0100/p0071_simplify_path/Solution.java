46a9c76143945d3158ee3ac48c13d2654f66d989
Dzmitry Charnavoki <Dzmitry_Charnavoki@epam.com>
2023-03-08 22:48:59 +0100

    71. Simplify Path

---
+++ src/main/java/problems/p0000_0100/p0071_simplify_path/Solution.java
@@ -0,0 +1,26 @@
+package problems.p0000_0100.p0071_simplify_path;
+
+import java.util.Stack;
+
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] dirs = path.split("/");
+
+        for (String dir : dirs) {
+            if (dir.equals(".") || dir.isEmpty()) {
+                continue;
+            }
+            if (dir.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(dir);
+            }
+
+        }
+
+        return "/" + String.join("/", stack);
+    }
+}
---
+++ src/test/java/problems/p0000_0100/p0071_simplify_path/SolutionTest.java
@@ -0,0 +1,29 @@
+package problems.p0000_0100.p0071_simplify_path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    void simplifyPath1() {
+        assertEquals("/home", new Solution().simplifyPath("/home/"));
+    }
+
+    @Test
+    void simplifyPath2() {
+        assertEquals("/", new Solution().simplifyPath("/../"));
+    }
+
+    @Test
+    void simplifyPath3() {
+        assertEquals("/home/foo", new Solution().simplifyPath("/home//foo/"));
+    }
+
+    @Test
+    void simplifyPath4() {
+        assertEquals("/", new Solution().simplifyPath("/vdfv/../"));
+    }
+
+}
