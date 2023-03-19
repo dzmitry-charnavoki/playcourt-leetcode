3d3d58853e78b97486e4e5c644539193f0e4f73c
Dzmitry Charnavoki <Dzmitry_Charnavoki@epam.com>
2023-03-19 12:26:25 +0100

    77. Combinations

--- src/main/java/problems/p0000_0100/p0077_combinations/Solution.java
+++ src/main/java/problems/p0000_0100/p0077_combinations/Solution.java
@@ -11,16 +11,16 @@ class Solution {
         return ans;
     }
 
-    private void dfs(int n, int k, int s, List<Integer> path, List<List<Integer>> ans) {
-        if (path.size() == k) {
-            ans.add(new ArrayList<>(path));
+    private void dfs(int n, int k, int s, List<Integer> combinations, List<List<Integer>> ans) {
+        if (combinations.size() == k) {
+            ans.add(new ArrayList<>(combinations));
             return;
         }
 
         for (int i = s; i <= n; ++i) {
-            path.add(i);
-            dfs(n, k, i + 1, path, ans);
-            path.remove(path.size() - 1);
+            combinations.add(i);
+            dfs(n, k, i + 1, combinations, ans);
+            combinations.remove(combinations.size() - 1);
         }
     }
 }
