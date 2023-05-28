636590981196f4c14a043dc4fccfdc4df39da9bf
Dzmitry Charnavoki <Dzmitry_Charnavoki@epam.com>
2023-05-28 14:19:05 +0200

    110. Balanced Binary Tree

--- src/test/java/problems/p0100_0200/p0110_balanced_binary_tree/SolutionTest.java
+++ src/test/java/problems/p0100_0200/p0110_balanced_binary_tree/SolutionTest.java
@@ -18,6 +18,34 @@ class SolutionTest {
     void isBalanced2() {
         assertFalse(new Solution()
             .isBalanced(TreeNode
+                .of(1, 2, 2, 3, null, null, 3, 4, null, null, 4)));
+    }
+
+    @Test
+    void isBalanced4() {
+        assertFalse(new Solution()
+            .isBalanced(TreeNode
+                .of(1, 2, 3, 4, 5, null, 6, 7, null, null, null, null, 8)));
+    }
+
+    @Test
+    void isBalanced_n1() {
+        assertTrue(new Solution()
+            .isBalanced(TreeNode
+                .of(3, 9, 20, null, null, null, 7)));
+    }
+
+    @Test
+    void isBalanced_n2() {
+        assertTrue(new Solution()
+            .isBalanced(TreeNode
+                .of(3, 9, 20, null, null, 15, null)));
+    }
+
+    @Test
+    void isBalanced_n3() {
+        assertFalse(new Solution()
+            .isBalanced(TreeNode
                 .of(1, 2, 2, 3, 3, null, null, 4, 4)));
     }
 
