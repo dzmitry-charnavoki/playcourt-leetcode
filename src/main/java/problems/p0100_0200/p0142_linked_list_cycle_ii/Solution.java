58aa5d08f55d3ecf76b2d7a6d99a5d3e85c68ecb
Dzmitry Charnavoki <Dzmitry_Charnavoki@epam.com>
2023-06-11 16:27:18 +0200

    142. Linked List Cycle II

---
+++ src/main/java/problems/p0100_0200/p0142_linked_list_cycle_ii/Solution.java
@@ -0,0 +1,28 @@
+package problems.p0100_0200.p0142_linked_list_cycle_ii;
+
+import utils.ListNode;
+
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) { //phase 1 - find cycle
+                // phase 2 - find node
+                slow = head;
+                while (fast != slow) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
